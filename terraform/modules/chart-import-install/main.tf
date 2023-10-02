8144091b2933e933a193f629e4e1e8a232e7a0fc
moi_m <moimonte5@gmail.com>
2023-10-02 19:39:43 +0200

    Citadel Challenge

--- terraform/modules/chart-import-install/main.tf
+++ terraform/modules/chart-import-install/main.tf
@@ -32,7 +32,7 @@ resource "null_resource" "import_charts" {
   	az acr import --name ${var.acr_server} --source ${var.source_acr_server}/${var.charts[count.index].chart_name}:${var.charts[count.index].chart_version} --subscription ${var.acr_server_subscription}
 	EOT
   }
-
+  #With this line we specify when this resource should be recreated. If you change the chart version in the charts variable, Terraform will detect this change and rerun the resource.
   triggers = {
 	chart_version = var.charts[count.index].chart_version
   }
@@ -42,22 +42,22 @@ resource "null_resource" "import_charts" {
 #Resource to install the helms in the repository “instance.azurecr.io”
 resource "helm_release" "chart_release" {
   count = length(var.charts)
- 
+  #We collect the data to use
   name      	= var.charts[count.index].chart_name
   namespace 	= var.charts[count.index].chart_namespace
   repository	= var.charts[count.index].chart_repository
   chart     	= var.charts[count.index].chart_name
   version   	= var.charts[count.index].chart_version
-
+  #We set the collected data
   set {
 	name  = var.charts[count.index].values.name
 	value = var.charts[count.index].values.value
   }
-
+  #We set the collected data
   set_sensitive {
 	name  = var.charts[count.index].sensitive_values.name
 	value = var.charts[count.index].sensitive_values.value
   }
-
+  #This ensures that charts are imported first before attempting to install them in AKS.
   depends_on = [null_resource.import_charts]
 }
